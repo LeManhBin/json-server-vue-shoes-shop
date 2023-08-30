e7a22584ee940adaf485929d37ba21a8b4b8b0fe
Lê Mạnh Bin <pacmam2k1@gmail.com>
2023-08-30 16:24:46 +0700

    fix cors 2

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 var express = require("express");
 var cors = require("cors");
-var note = express();
-
-note.use(cors());
+app.use(cors());
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
 
 const app = jsonServer.create();
+
 const router = jsonServer.router("db.json");
 
 // /!\ Bind the router db to the app
